c990ce824ca236d8a03e67bee1d2f3e59de38c23
crpistillo <cpistillo@fi.uba.ar>
2020-06-23 03:08:37 -0300

    Apuntes

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -4,10 +4,14 @@ import {Text, View, Button} from "react-native";
 class HomeScreen extends Component {
     render() {
         return(
+            //TODO: View es analogo al "div" de React
+            //TODO: Una view es cualquier cosa que sea un contenedor de elementos o vacio
             <View>
                 <Text>Inicio</Text>
                 <Button onPress={() => this.props.navigation.navigate('Profile')} title={"Mi Perfil"}/>
             </View>
+            //TODO: Cuando apriteo el boton navego hasta "profile"
+            //TODO: Los Text, Button, y en geral todos los elementos son distintos a los de html
         )
     }
 }
